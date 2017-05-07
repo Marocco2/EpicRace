0b6d8410556147d80ca7371767e3c049be4752ad
Marocco2 <marocco2@live.it>
2017-05-07 12:31:37 +0200

    Revert "Revert "revert test""
    This reverts commit a92265556e76f8c7eeaa308e5ea5dd3f119300af.

--- EpicRace.py
+++ EpicRace.py
@@ -401,10 +401,10 @@ def acUpdate(deltaT):
     if ((sessionTime <= 0 and session <= 2) or session == 3) and lenqueue == 0 and (
                                 isPlayingStartRace or
                                 isPlayingBeforeRace or
-                                isPlayingSuspense or
-                                isPlayingAfterRace or
-                                isPlayingOvertake or
-                                isPlayingHotlap):
+                            isPlayingSuspense or
+                        isPlayingAfterRace or
+                    isPlayingOvertake or
+                isPlayingHotlap):
         wait_a += 1
         if lenqueue == 0 and wait_a == 100:
             isPlayingStartRace = isPlayingBeforeRace = isPlayingSuspense = \
@@ -430,8 +430,7 @@ def acUpdate(deltaT):
                    "\nisPlayingSuspense: " + str(isPlayingSuspense) +
                    "\nisPlayingAfterRace: " + str(isPlayingAfterRace) +
                    "\nisPlayingHotlap: " + str(isPlayingHotlap) +
-                   "\nisPlayingOvertake: " + str(isPlayingOvertake) +
-                   "\nTest autoupdate")
+                   "\nisPlayingOvertake: " + str(isPlayingOvertake))
 
     if overflow < 50:
 
@@ -533,7 +532,7 @@ def acUpdate(deltaT):
 
             if session == 3:  # Hotlap session
                 if enable_hotlap:
-                    if lastlap == bestlap and lap != lastlap and completedLaps > 1 \
+                    if lastlap == bestlap and lap != lastlap and completedLaps > 1\
                             and not hot_once and not isPlayingHotlap:
                         lap = lastlap
                         ac.log(log + "Hotlap detected")
